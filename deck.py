d9d67f8a208c2193954eaa09257d55f8e62624ff
Jesus <sisniega.serrano@gmail.com>
2026-01-31 18:15:38 -0500

    Refactor deck generation and shuffle functions
    Refactor card deck generation and shuffling methods.

--- deck.py
+++ deck.py
@@ -1,78 +1,93 @@
 ## Clubs, Hearts, Spades, Diamonds - 13 cards each.
 import random
 
-n = 52 ## number of cards.
-posNum = []
-for i in range(1, n + 1): ## Sigma notation which counts by intervals of 1, starting @ 1, to n.
-    posNum += [i] ## Adds every num (1-n) to empty list.
 
-deckset = []  ## holds avaliable cards. 
-m = 13 ## number of cards per suit.
-while posNum != []:
-    for i in range(1, m + 1):
-        deckset += ["c"+str(i)]
-        del posNum[0]
-    for i in range(1, m + 1):
-        deckset += ["h"+str(i)]
-        del posNum[0]
-    for i in range(1, m + 1):
-        deckset += ["s"+str(i)]
-        del posNum[0]
-    for i in range(1, m + 1):
-        deckset += ["d"+str(i)]
-        del posNum[0]
+def gen():
 
+    n = 52 ## number of cards.
+    posNum = []
+    for i in range(1, n + 1): ## Sigma notation which counts by intervals of 1, starting @ 1, to n.
+        posNum += [i] ## Adds every num (1-n) to empty list.
 
+    m = 13 ## number of cards per suit.
 
-def shuffle():
+#### FOR FUTURE REFERENCE: THIS COULD BE CODED MUCH CLEANER.
+    deckset = []
+
+    while posNum != []:
+        for i in range(1, m + 1):
+            deckset += ["c" + str(i)]
+            del posNum[0]
+        for i in range(1, m + 1):
+            deckset += ["h" + str(i)]
+            del posNum[0]
+        for i in range(1, m + 1):
+            deckset += ["s" + str(i)]
+            del posNum[0]
+        for i in range(1, m + 1):
+            deckset += ["d" + str(i)]
+            del posNum[0]
+
+    deck = []
+
+    def unpack(deckset):
+        cardID = str(drawCard(deckset, 0)) # creates string obj from list element.
+        card = []
+
+        if cardID[0] == 'c': 
+            card += ["Black"]
+            card += ["Clubs"]
+        elif cardID[0] == 'h':
+            card += ["Red"]
+            card += ['Hearts']
+        elif cardID[0] == 's':
+            card += ["Black"]
+            card += ['Spades']
+        else:
+            card += ["Red"]
+            card += ['Diamonds']
+        
+        if len(cardID) == 3:
+            num = 10 + int(cardID[2])
+            card += [num]
+        else: 
+            num = int(cardID[1])
+            card += [num]
+
+        return card
+
+    while deckset != []:
+        deck.append(unpack(deckset))
+#### ONLY KEEPING INEFFECIENT CODE TO HIGHLIGHT MY ABILITY TO WORK WITH PREVIOUSLY COMPLETED PROJECTS.
+
+    return deck
+
+def shuffle(deck):
     o = 0
     shuffledDeckset = []
     
-    for o in range(0, len(deckset), 1):
-        shuffledDeckset.append(drawCard(random.randrange(0, len(deckset), 1)))
+    for o in range(0, len(deck), 1):
+        shuffledDeckset.append(drawCard(deck, random.randrange(0, len(deck), 1)))
 
-    
+    # adds each 'card' arr indiv, in new order, to the previous 'deck' list obj.
     for o in range(0, len(shuffledDeckset), 1):
-        deckset.append(shuffledDeckset[o])
+        deck.append(shuffledDeckset[o])
 
-def drawCard(pos): ##  accessor method.
-    card = str(deckset[pos]) ## creates string obj from list element.
-    cardInfo = []
-
-    if card[0] == 'c': 
-        cardInfo += ["Black"]
-        cardInfo += ["Clubs"]
-    elif card[0] == 'h':
-        cardInfo += ["Red"]
-        cardInfo += ['Hearts']
-    elif card[0] == 's':
-        cardInfo += ["Black"]
-        cardInfo += ['Spades']
-    else:
-        cardInfo += ["Red"]
-        cardInfo += ['Diamonds']
-    
-    if len(card) == 3:
-        num = 10 + int(card[2])
-        cardInfo += [num]
-    else: 
-        num = int(card[1])
-        cardInfo += [num]
-
-
-    del deckset[pos]
-    return cardInfo
+# deck list obj's card arr accessor method.
+def drawCard(deckList, pos):
+    card = deckList[pos]
+    del deckList[pos]
+    return card
 
 
 ## bug checkerrr
 '''
 
+dih = gen()
+print(dih)
+print("break! \n")
+shuffle(dih)
+print(dih)
+print(len(dih))
 
-x = 17
-#print(deckset[x])
-#print(draw_Card(x))
-print(deckset)
-shuffle()
-print("\n")
-print(deckset)
 #'''
