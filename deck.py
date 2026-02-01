cf2f52c689f3ac3dbb0ba09f5d8d4864c612849f
Jesus <sisniega.serrano@gmail.com>
2026-02-01 16:52:26 -0500

    Updated documentation.

--- deck.py
+++ deck.py
@@ -1,17 +1,18 @@
 ## Clubs, Hearts, Spades, Diamonds - 13 cards each.
 import random
 
-
+# Generates a "deck", a list object, containing "cards", arrays with a singular card's info specified.
 def gen():
+    n = 52 # The number of cards desired. 
+    # Setting n to > 52 makes more cards generate in a predictable manner. 
+    # I recommend generting multiples of 52.
 
-    n = 52 ## number of cards.
     posNum = []
     for i in range(1, n + 1): ## Sigma notation which counts by intervals of 1, starting @ 1, to n.
         posNum += [i] ## Adds every num (1-n) to empty list.
 
-    m = 13 ## number of cards per suit.
-
 #### FOR FUTURE REFERENCE: THIS COULD BE CODED MUCH CLEANER.
+    m = 13 # The number of cards per suit.
     deckset = []
 
     while posNum != []:
@@ -62,6 +63,7 @@ def gen():
 
     return deck
 
+# Method shuffles arrays contained in deck list.
 def shuffle(deck):
     o = 0
     shuffledDeckset = []
@@ -73,7 +75,7 @@ def shuffle(deck):
     for o in range(0, len(shuffledDeckset), 1):
         deck.append(shuffledDeckset[o])
 
-# deck list obj's card arr accessor method.
+# Method for quickly pulling an array containing a singular card's info from stack.
 def drawCard(deckList, pos):
     card = deckList[pos]
     del deckList[pos]
